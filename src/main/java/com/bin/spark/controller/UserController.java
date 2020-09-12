92913c2ed6036959318ed7e892bf6468ca3ee35d
fgu30 <fgu30@gatech.edu>
2020-09-13 04:33:58 +0800

    llb: 集成thymeleaf

--- src/main/java/com/bin/spark/controller/UserController.java
+++ src/main/java/com/bin/spark/controller/UserController.java
@@ -4,22 +4,30 @@ import com.bin.spark.common.ResponseVo;
 import com.bin.spark.model.UserModel;
 import com.bin.spark.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by 斌~
  * 2020/9/12 15:57
  * @author mac
  */
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    @GetMapping("/test")
+    public ModelAndView test(){
+        return new ModelAndView("/test.html");
+    }
+
     @GetMapping("/{userId}")
+    @ResponseBody
     public ResponseVo<UserModel> getUserInfo (@PathVariable Integer userId ){
         UserModel user = userService.getUser(userId);
         if(user ==null){
