4e728fecaf1f8b639acb2ea0333fb72b1793a812
fgu30 <fgu30@gatech.edu>
2020-09-13 20:38:03 +0800

    llb: 首页、登录以及注册页面

---
+++ src/main/java/com/bin/spark/controller/admin/AdminController.java
@@ -0,0 +1,16 @@
+package com.bin.spark.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sun.misc.Contended;
+
+/**
+ * @author 斌~
+ * @version 1.0
+ * @date 2020/9/13 7:47 下午
+ */
+@Controller("/admin/admin")
+@RequestMapping("/admin/admin")
+public class AdminController {
+
+}
